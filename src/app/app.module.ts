71448bc11950a0fd503d93906fe50c40d81878ad
alejandroQtx <qualitrain@gmail.com>
2024-10-12 10:00:33 -0600

    componente adios ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestObservableComponent } from './test-observable/test-observable.component';
+import { AdiosComponent } from './adios/adios.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestObservableComponent
+    TestObservableComponent,
+    AdiosComponent
   ],
   imports: [
     BrowserModule,
